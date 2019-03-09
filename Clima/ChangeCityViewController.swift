09611223b9b02045ceb8ee8ba88e30d422945751
Nick Deloglos <nick.deloglos@gmail.com>
2019-03-09 11:46:01 -0500

    Initial Commit

---
+++ Clima/ChangeCityViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ChangeCityViewController.swift
+//  WeatherApp
+//
+
+
+import UIKit
+
+
+
+
+
+class ChangeCityViewController: UIViewController {
+    
+   
+    @IBOutlet weak var changeCityTextField: UITextField!
+
+    
+    @IBAction func getWeatherPressed(_ sender: AnyObject) {
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+
+    
+    @IBAction func backButtonPressed(_ sender: AnyObject) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
