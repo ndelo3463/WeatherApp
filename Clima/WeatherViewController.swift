73fbd4f18dab883b48312009911454c837bef120
Nick Deloglos <nick.deloglos@gmail.com>
2019-03-09 13:00:23 -0500

    Update 2

--- Clima/WeatherViewController.swift
+++ Clima/WeatherViewController.swift
@@ -5,13 +5,17 @@
 
 
 import UIKit
+import CoreLocation
 
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, CLLocationManagerDelegate{
     
     
     let WEATHER_URL = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APP_ID}"
     let APP_ID = "30cc3152886a5e4bd4ed0a6da491232c"
+    
+    let locationManager = CLLocationManager()
+    
     
 
 
@@ -27,6 +31,9 @@ class WeatherViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+        locationManager.requestWhenInUseAuthorization()
         
     
     
